e592d75cd3b43e8a0497d63dccc34aea88d03838
rayGreycloud <rayGreycloud@gmail.com>
2018-04-13 11:30:21 -0700

    Add comments

--- ethereum/contracts/zombieattack.sol
+++ ethereum/contracts/zombieattack.sol
@@ -2,19 +2,36 @@ pragma solidity ^0.4.19;
 
 import "./zombiehelper.sol";
 
+/**
+ * @title ZombieBattle
+ */
 contract ZombieBattle is ZombieHelper {
-  
+  // Emits results of the attack 
   event AttackResult(uint indexed attackingZombie, uint indexed enemyZombie, bool attackSuccess);
   
+  // Initialize nonce used to generate pseudo-random modulus  
   uint randNonce = 0;
+  // Set probability of attack success 
   uint attackVictoryProbability = 70;
+  // Initialize success flag
   bool attackSuccess;
 
+  /**
+  * @dev Function to generate pseudo-random number representing attack results.
+  * @param uint _modulus Number used in rng process.
+  * @return pseudo-random uint.
+  */
   function randMod(uint _modulus) internal returns(uint) {
     randNonce = randNonce.add(1);
     return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
   }
-
+  
+  /**
+  * @dev Function to simulate attack on another zombie and process results 
+  * @param _zombieId The uint id of attacking zombie.
+  * @param _targetId The uint id of zombie target.
+  * @modifier onlyOwnerOf restricts to owner of attacking zombie
+  */
   function attack(uint _zombieId, uint _targetId) external onlyOwnerOf(_zombieId) {
     Zombie storage myZombie = zombies[_zombieId];
     Zombie storage enemyZombie = zombies[_targetId];
